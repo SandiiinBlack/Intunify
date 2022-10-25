6fb59312c1457559810f50a2b7c1a2477c673292
Emil Kloeden <emil.kloeden@satac.edu.au>
2022-10-25 15:45:11 +1030

    FEAT: Added -X/--exclude_file parameter (mutually exclusive with -x --exclude) to bulk generator.

--- bulk_application_installer_generator.py
+++ bulk_application_installer_generator.py
@@ -7,7 +7,9 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument('-i', '--infile', type=str, required=True, help="path to JSON input file")
     parser.add_argument('-o', '--outfolder', type=str, required=True, help="path to place intunewin apps")
-    parser.add_argument('-x', '--exclude', type=str, nargs="*", help=" List of space-separated WingetId's to exclude. Case insensitive.")
+    exclusion_group = parser.add_mutually_exclusive_group()
+    exclusion_group.add_argument('-x', '--exclude', type=str, nargs="*", help="list of space-separated WingetId's to exclude. Case insensitive.")
+    exclusion_group.add_argument('-X', '--excludefile', type=str, help="path to a json file containing an array of WingetIds to exclude. Exclusion is case insensitive.")
     return parser.parse_args()
 
 
@@ -29,6 +31,7 @@ def main():
             raise ValueError(f"All applications must contain either a 'file_path' or a 'registry_key' key, not both")
     
     # Remove excluded ids
+    # parsed as command line arguments
     if args.exclude:
         lower_cased_ids_to_exclude = [winget_id.lower() for winget_id in args.exclude]
         applications = [
@@ -37,6 +40,19 @@ def main():
             in applications
             if app["winget_id"].lower() not in lower_cased_ids_to_exclude
         ]
+
+    # ...or as a file path
+    elif args.excludefile:
+        p = Path(args.excludefile)
+        with p.open('r') as f:
+            exclusions = json.load(f)
+            lower_cased_ids_to_exclude = [winget_id.lower() for winget_id in exclusions]
+            applications = [
+                app
+                for app
+                in applications
+                if app["winget_id"].lower() not in lower_cased_ids_to_exclude
+            ]
 
     # Build intunewin app
     for application in applications:
